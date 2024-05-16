15550378263fe79cd720374bd60c8ca7002117b7
Evlusha <106895767+Evlusha@users.noreply.github.com>
2024-05-16 23:56:31 +0800

    Update zd2.py

--- zd2.py
+++ zd2.py
@@ -1,26 +1,28 @@
-def decodeString(s):
-    def helper(i):
-        res = ""
-        while i < len(s) and s[i] != ']':
-            if not s[i].isdigit():
-                res += s[i]
-                i += 1
-            else:
-                k = 0
-                while i < len(s) and s[i].isdigit():
-                    k = k * 10 + int(s[i])
-                    i += 1
-                i += 1  # Skip '['
-                decodedStr, i = helper(i)
-                i += 1  # Skip ']'
-                res += k * decodedStr
-            i += 1
-        return res, i
+def decode_string(s, i):
+    res = ""
+    
+    while i[0] < len(s) and s[i[0]] != ']':
+        if not s[i[0]].isdigit():
+            res += s[i[0]]
+            i[0] += 1
+        else:
+            k = 0
+            while i[0] < len(s) and s[i[0]].isdigit():
+                k = k * 10 + int(s[i[0]])
+                i[0] += 1
+            
+            i[0] += 1  # Пропускаем '['
+            decoded_str = decode_string(s, i)
+            i[0] += 1  # Пропускаем ']'
+            
+            res += decoded_str * k
 
-    i = 0
-    result, _ = helper(i)
-    return result
+    return res
 
-print("Введите:")
-s = input()
-print("Вывод:", decodeString(s))
+def decode_string_main(s):
+    i = [0]
+    return decode_string(s, i)
+
+if __name__ == "__main__":
+    s = input("Введите: ")
+    print("Вывод:", decode_string_main(s))
